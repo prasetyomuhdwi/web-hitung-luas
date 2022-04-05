84ae59664417fa5ede2fd5529dd9b48f076ff120
prasetyomuhdwi <lenovpras110@gmail.com>
2022-04-05 21:24:51 +0700

    [components/navbar.php] Add navbar component and implement to index

---
+++ components/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="segitiga.php">Segitiga</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="persegi.php">Persegi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="lingkaran.php">Lingkaran</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -11,26 +11,8 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link fw-bold" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="segitiga.php">Segitiga</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="persegi.php">Persegi</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="lingkaran.php">Lingkaran</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <!-- menyertakan file component navbar -->
+    <?php include_once "components/navbar.php" ?>
 
     <div class="container py-4">
 
