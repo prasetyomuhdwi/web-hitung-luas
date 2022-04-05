d64366d0ae8eb1c7a10a93738f70255cab1ad603
prasetyomuhdwi <lenovpras110@gmail.com>
2022-04-05 21:21:38 +0700

    [index.php] Add simple dashboard

---
+++ index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Perhitungan Luas Bangunan</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="segitiga.php">Segitiga</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="persegi.php">Persegi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="lingkaran.php">Lingkaran</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container py-4">
+
+        <!-- Title website -->
+        <div class="p-5 mb-3 bg-light rounded">
+            <div class="container text-dark text-center">
+                <h1 class="display-6 fw-bolder">Website Perhitungan Luas Bangun Datar</h1>
+                <p>Website ini dapat digunakan untuk menghitung luas bangun datar segitiga, persegi, dan lingkaran </p>
+            </div>
+        </div>
+
+        <!-- Card Analisis Total dan Persentase -->
+        <div class="card p-3 bg-light mb-3">
+            <div class="card-body">
+                <div class="row mb-3">
+
+                    <!-- Segitiga -->
+                    <div class="col-md-6">
+                        <div class="card text-center">
+                            <div class="card-header bg-dark text-light">
+                                Perhitungan Segitiga Yang Telah Dilakukan
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <div class="col-md-6">
+                                        <div class="card">
+                                            <b>Total</b>
+                                            <h1>12</h1>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="card">
+                                            <b>Persentase</b>
+                                            <h1>12%</h1>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <!-- Persegi -->
+                    <div class="col-md-6">
+                        <div class="card text-center">
+                            <div class="card-header bg-dark text-light">
+                                Perhitungan Persegi Yang Telah Dilakukan
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <div class="col-md-6">
+                                        <div class="card">
+                                            <b>Total</b>
+                                            <h1>12</h1>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="card">
+                                            <b>Persentase</b>
+                                            <h1>12%</h1>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Lingkaran -->
+                <div class="d-flex justify-content-center">
+                    <div class="col-md-6 mb-3">
+                        <div class="card text-center">
+                            <div class="card-header bg-dark text-light">
+                                Perhitungan Lingkaran Yang Telah Dilakukan
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+
+                                    <div class="col-md-6">
+                                        <div class="card">
+                                            <b>Total</b>
+                                            <h1>12</h1>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="card">
+                                            <b>Persentase</b>
+                                            <h1>12%</h1>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <script src="./assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
