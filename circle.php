db2acfccba8f61d48c7829f839550a330754de8d
prasetyomuhdwi <lenovpras110@gmail.com>
2022-04-05 22:12:03 +0700

    [circle.php] Add square and circle page contain table and button

---
+++ circle.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Perhitungan Luas Bangunan</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <?php include_once "components/navbar.php" ?>
+
+    <div class="container py-4">
+
+        <!-- Title website -->
+        <div class="p-5 mb-3 bg-light rounded">
+            <div class="container text-dark">
+                <h1 class="display-6 fw-bolder text-uppercase">Lingkaran</h1>
+                <p>List data lingkaran yang sudah dilakukan</p>
+            </div>
+
+            <div class="container">
+                <a href="tambah_lingkaran.php" class="btn btn-primary float-end" role="button">Perhitungan Baru</a>
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr class="text-center">
+                            <th scope="col">No</th>
+                            <th scope="col">Nama Siswa</th>
+                            <th scope="col">Jari-jari (r)</th>
+                            <th scope="col">Hasil</th>
+                            <th scope="col">Tanggal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr class='text-center'>
+                            <td colspan='5' class='p-4 bg-light text-center 
+                            text-danger fw-bold'>Data Tidak Ditemukan</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+
+        <script src="./assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- components/navbar.php
+++ components/navbar.php
@@ -6,13 +6,13 @@
                     <a class="nav-link fw-bold" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="​triangle.php">Segitiga</a>
+                    <a class="nav-link" href="triangle.php">Segitiga</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="square.php">Persegi</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " href="​​circle.php">Lingkaran</a>
+                    <a class="nav-link " href="circle.php">Lingkaran</a>
                 </li>
             </ul>
         </div>
